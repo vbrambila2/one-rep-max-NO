26e9d14588c5e4de30e6c4f10d10a26bcb9752fa
Victor Brambila <victorbrambila22@gmail.com>
2021-05-17 10:35:24 -0700

    add page setup

---
+++ src/containers/MovementPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const MovementPage = () => {
+    return (
+        <div>
+            <Header title="Movement Page" />
+        </div>
+    );
+};
+ export default MovementPage;
