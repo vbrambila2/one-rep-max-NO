17f6ec285b5bebb1710fc28b53a98cae6449c3b8
Victor Brambila <victorbrambila22@gmail.com>
2021-06-23 13:06:43 -0700

    added basename to router

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Router history={history}>
+                <Router history={history} basename="/one-rep-max/">
                     <Switch>
                         <Route exact path="/" render={ () => <HomePage title="One Rep - Home" /> } ></Route>
                         <Route exact path="/movement/:name/:weight" render={ () => <MovementPage title="One Rep - Movement" /> } ></Route>
